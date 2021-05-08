04ec35350569c1e9beeefe0a943e47eed8aff259
Arjun Lather <devendermalik009@gmail.com>
2021-05-08 17:12:35 +0530

    Remove next dependency from server

--- src/server/server.js
+++ src/server/server.js
@@ -19,21 +19,18 @@ const apiPaths = {
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-app.prepare().then(() => {
-    const server = express()
 
-    if (isDevelopment) {
-        server.use('/api', createProxyMiddleware(apiPaths['/api']));
-    }
+const server = express()
 
-    server.all('*', (req, res) => {
-        return handle(req, res)
-    })
+if (isDevelopment) {
+    server.use('/api', createProxyMiddleware(apiPaths['/api']));
+}
 
-    server.listen(port, (err) => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-    })
-}).catch(err => {
-    console.log('Error:::::', err)
+server.all('*', (req, res) => {
+    return handle(req, res)
 })
+
+server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+});
