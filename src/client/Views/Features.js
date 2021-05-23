0b3af3c8bd4f4308cf1b02c1d12fa7e2f946cb43
Sidhant D <sidhant.d@ingeniumedu.com>
2021-05-23 22:00:42 +0530

    built professionals component

--- src/client/Views/Features.js
+++ src/client/Views/Features.js
@@ -1,17 +1,53 @@
+import { useEffect, useState } from 'react';
+
+const NumberCounter = (props) => {
+  const { number, duration } = props;
+  const [count, setCount] = useState('0');
+
+  useEffect(() => {
+    let start = 0;
+    const end = parseInt(number.substring(0, 3));
+    if (start === end) return;
+
+    let totalMilSecDur = parseInt(duration);
+    let incrementTime = (totalMilSecDur / end) * 1000;
+
+    let timer = setInterval(() => {
+      start += 1;
+      setCount(String(start) + number.substring(3));
+      if (start === end) clearInterval(timer);
+    }, incrementTime);
+  }, [number, duration]);
+
+  return (
+    <span
+      style={{
+        fontSize: '24px',
+        fontWeight: '800',
+        color: 'var(--primary)',
+      }}
+    >
+      {count}+
+    </span>
+  );
+};
+
 export function Features() {
   return (
-    <section id="features" class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="features-text section-header text-center">
+    <section id="features" className="section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="features-text section-header text-center">
               <div>
-                <h2 class="section-title">Services We Provide</h2>
-                <div class="desc-text">
+                <h2 className="section-title">
+                  Curated Professionals On our platform
+                </h2>
+                <div className="desc-text">
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do <br />
-                    eiusmod tempor incididunt ut labore et dolore.
+                    A few numbers to show the abundance of talent
+                    <br />
+                    at the top-guns platform
                   </p>
                 </div>
               </div>
@@ -19,108 +55,71 @@ export function Features() {
           </div>
         </div>
 
-        <div class="row featured-bg">
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item featured-border1">
-              <div class="feature-icon float-left">
-                <i class="lni-coffee-cup"></i>
+        <div className="row featured-bg">
+          {[
+            {
+              key: 1,
+              tech: 'ReactJS',
+              text: 'professionals building beautiful UIs.',
+              number: 1000,
+              icon: 'lni lni-github',
+            },
+            {
+              key: 2,
+              tech: 'NodeJS',
+              text: 'professionals building complex backends.',
+              number: 250,
+              icon: 'lni lni-stackoverflow',
+            },
+            {
+              key: 3,
+              tech: 'Designers',
+              text: 'professionals doing super creative work.',
+              number: 1000,
+              icon: 'lni lni-behance',
+            },
+            {
+              key: 4,
+              tech: 'Java',
+              text: 'professionals sipping coffee and doing serious work.',
+              number: 1000,
+              icon: 'lni lni-java',
+            },
+            {
+              key: 5,
+              tech: 'Android',
+              text: 'professionals making smart mobile apps',
+              number: 1000,
+              icon: 'lni lni-android',
+            },
+            {
+              key: 6,
+              tech: 'Product Managers',
+              text: 'professionals handling complex teams',
+              number: 1000,
+              icon: 'lni lni-slack',
+            },
+          ].map((elem) => {
+            return (
+              <div className="col-lg-6 col-md-6 col-xs-12 p-0" key={elem.key}>
+                <div className="feature-item featured-border1">
+                  <div className="feature-icon float-left">
+                    <i className={elem.icon}></i>
+                  </div>
+                  <div className="feature-info float-left">
+                    <h4>{elem.tech}</h4>
+                    <p>
+                      <NumberCounter
+                        number={elem.number.toString()}
+                        duration={5}
+                      />{' '}
+                      {elem.text}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div class="feature-info float-left">
-                <h4>Easy to Customize</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item featured-border2">
-              <div class="feature-icon float-left">
-                <i class="lni-briefcase"></i>
-              </div>
-              <div class="feature-info float-left">
-                <h4>Business Template</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item featured-border1">
-              <div class="feature-icon float-left">
-                <i class="lni-invention"></i>
-              </div>
-              <div class="feature-info float-left">
-                <h4>Clean & Trendy Design</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item featured-border2">
-              <div class="feature-icon float-left">
-                <i class="lni-layers"></i>
-              </div>
-              <div class="feature-info float-left">
-                <h4>Tons of Sections</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item featured-border3">
-              <div class="feature-icon float-left">
-                <i class="lni-reload"></i>
-              </div>
-              <div class="feature-info float-left">
-                <h4>Free Future Updates</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-xs-12 p-0">
-            <div class="feature-item">
-              <div class="feature-icon float-left">
-                <i class="lni-support"></i>
-              </div>
-              <div class="feature-info float-left">
-                <h4>Premier Support</h4>
-                <p>
-                  Producing long lasting organic SEO results for <br />
-                  brand of different kinds for more than a decade,
-                  <br />
-                  we understand that’s your.
-                </p>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </section>
--- src/client/Views/Services.js
+++ src/client/Views/Services.js
@@ -6,29 +6,39 @@ export function Services() {
           {[
             {
               key: 1,
-              icon: 'lni lni-list',
+              icon: '/img/services/step1.png',
               heading: 'Create Requirement',
               text: 'Create a requirement by filling a basic questionairre. It only take a couple of minutes, we promise!',
             },
+            //    { key: 5, arrow: '/img/services/arrow1.png', arrow: true },
             {
               key: 2,
-              icon: 'lni lni-network',
+              icon: '/img/services/step2.png',
               heading: 'Choose Professionals',
               text: 'Choose Proffessionals from a curated list of vetted talent, with detailed assessment, reliability and efficiency scores.',
             },
 
+            //    { key: 6, arrow: '/img/services/arrow2.png', arrow: true },
             {
               key: 3,
-              icon: 'lni lni-cloud-check',
+              icon: '/img/services/step3.png',
+              heading: 'Build Your Team',
+              text: 'Have a chat with the shortlisted candidates for culture fit & comfort. We shall handle the rest.',
+            },
+
+            //     { key: 7, arrow: '/img/services/arrow3.png', arrow: true },
+            {
+              key: 4,
+              icon: '/img/services/step4.png',
               heading: 'Build Your Team',
               text: 'Have a chat with the shortlisted candidates for culture fit & comfort. We shall handle the rest.',
             },
           ].map((elem) => {
             return (
-              <div className="col-lg-4 col-md-6 col-xs-12" key={elem.key}>
+              <div className="col-lg-3 col-md-6 col-xs-12" key={elem.key}>
                 <div className="services-item text-center">
-                  <div className="icon">
-                    <i class={elem.icon}></i>
+                  <div className="">
+                    <img src={elem.icon} />
                   </div>
                   <h4>{elem.heading}</h4>
                   <p>{elem.text}</p>
