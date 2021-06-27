c8634e99c8162b7c41b5b2fefd00a6a396b3fb3d
Arjun Lather <arjunslather@gmail.com>
2021-06-27 22:53:07 +0530

    Add state change to DevResources

--- src/client/pages/hire/quiz/dev-resources.js
+++ src/client/pages/hire/quiz/dev-resources.js
@@ -3,68 +3,43 @@ import Button from '@material-ui/core/Button';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import Head from 'next/head';
 import React from 'react';
-
-const renderRadioButtonQuestion = question => {
-  return (<div key={question.name}>
-    <FormLabel component="legend">{question.text}</FormLabel>
-    <RadioGroup aria-label={question.name} name={question.name} >
-      {question.options.map(option => { return <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} /> })}
-    </RadioGroup></div>
-  );
-}
-
-const renderRadioButtonQuestions = questions => {
-  return (questions.map(question => renderRadioButtonQuestion(question)));
-}
-
-
-const questions = [
-  {
-    text: "How many developers do you want to hire?",
-    name: "developersCount",
-    options: [
-      {
-        label: "One developer",
-        value: "one_dev"
-      },
-      {
-        label: "I need a team of developers",
-        value: "dev_team"
-      },
-      {
-        label: "I'll decide later",
-        value: "undecided"
-      },
-    ]
-  },
-  {
-    text: "How long do you need the developer?",
-    name: "developersCount",
-    options: [
-      {
-        label: "Less than 1 month",
-        value: "less_than_month"
-      }, {
-        label: "1 to 3 months",
-        value: "one_to_three_months"
-      },
-      {
-        label: "3 to 6 months",
-        value: "three_to_six_months"
-      },
-      {
-        label: "Longer than 6 months",
-        value: "more_than_six_months"
-      },
-      {
-        label: "I'll decide later",
-        value: "undecided"
-      },
-    ]
-  }
-]
+import { useAppContext } from '../../../context/state';
 
 const DevResources = () => {
+  const { applicationState, setApplicationState } = useAppContext();
+
+  const handleChange = questionName => event => {
+
+    const updateQuestionIndex = applicationState.questions.findIndex(question => question.name === questionName);
+    console.log("questionName: ", questionName, " ,updateQuestionIndex: ", updateQuestionIndex)
+
+    if (updateQuestionIndex > -1) {
+      const updatedQuestion = { ...applicationState.questions[updateQuestionIndex], answer: event.target.value };
+
+      setApplicationState({
+        ...applicationState,
+        questions: [
+          ...applicationState.questions.slice(0, updateQuestionIndex),
+          updatedQuestion,
+          ...applicationState.questions.slice(updateQuestionIndex + 1),
+        ],
+      });
+    }
+  }
+
+  const renderRadioButtonQuestion = question => {
+    return (<div key={question.name}>
+      <FormLabel component="legend">{question.text}</FormLabel>
+      <RadioGroup aria-label={question.name} name={question.name} onChange={handleChange(question.name)}>
+        {question.options.map(option => <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} />)}
+      </RadioGroup></div>
+    );
+  }
+
+  const renderRadioButtonQuestions = questions => {
+    return (questions.map(question => renderRadioButtonQuestion(question)));
+  }
+
   return (
     <div>
       <Head>
@@ -81,17 +56,15 @@ const DevResources = () => {
           justifyContent: 'center',
         }}>
           <FormControl component="fieldset">
-            {renderRadioButtonQuestions(questions)}
-
+            {renderRadioButtonQuestions(applicationState.questions)}
             <IconButton aria-label="back">
               <ArrowBackIosIcon />
             </IconButton>
             <Button variant="contained" color="primary">
               Next
-          </Button>
+            </Button>
           </FormControl>
         </div>
-        hello
       </main>
     </div>
   );
